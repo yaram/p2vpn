678bae00c836989f0da4eac3921efbf82ae866ea
Wayde Reitsma <pootisspencerherenao@gmail.com>
2021-08-30 00:30:51 +1000

    Add basic GUI

--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,7 @@
 #include "wintun.h"
 #define CBASE64_IMPLEMENTATION
 #include "cbase64.h"
+#include "ui.h"
 
 static WINTUN_ENUM_ADAPTERS_FUNC WintunEnumAdapters;
 static WINTUN_CREATE_ADAPTER_FUNC WintunCreateAdapter;
@@ -28,72 +29,6 @@ static WINTUN_ALLOCATE_SEND_PACKET_FUNC WintunAllocateSendPacket;
 static WINTUN_SEND_PACKET_FUNC WintunSendPacket;
 static WINTUN_END_SESSION_FUNC WintunEndSession;
 static WINTUN_FREE_ADAPTER_FUNC WintunFreeAdapter;
-
-struct JuiceParameters {
-    WINTUN_SESSION_HANDLE wintun_session;
-
-    HANDLE gathering_done_event;
-};
-
-static void on_gathering_done(juice_agent_t *agent, void *user_ptr) {
-    auto parameters = (JuiceParameters*)user_ptr;
-
-    SetEvent(parameters->gathering_done_event);
-}
-
-static void on_recv(juice_agent_t *agent, const char *data, size_t size, void *user_ptr) {
-    auto parameters = (JuiceParameters*)user_ptr;
-
-    auto packet = WintunAllocateSendPacket(parameters->wintun_session, (DWORD)size);
-
-    memcpy(packet, data, size);
-
-    WintunSendPacket(parameters->wintun_session, packet);
-}
-
-static void readline(char *buffer, size_t buffer_size) {
-    size_t character_index = 0;
-    while(character_index != buffer_size - 1) {
-        auto character = getc(stdin);
-
-        if(character == -1) {
-            fprintf(stderr, "\nERROR: Unexpected EOF in stdin\n");
-
-            exit(1);
-        }
-
-        if(character == '\r') {
-            character = getc(stdin);
-
-            if(character != '\n') {
-                ungetc(character, stdin);
-            }
-        } else if(character == '\n') {
-            break;
-        }
-
-        buffer[character_index] = (char)character;
-
-        character_index += 1;
-    }
-
-    buffer[character_index] = '\0';
-}
-
-struct EnumerationParameters {
-    bool adapter_found;
-    WCHAR adapter_name[MAX_ADAPTER_NAME];
-};
-
-static BOOL CALLBACK wintun_adapter_enumeration_callback(WINTUN_ADAPTER_HANDLE adapter, LPARAM parameter) {
-    auto parameters = (EnumerationParameters*)parameter;
-
-    parameters->adapter_found = true;
-
-    WintunGetAdapterName(adapter, parameters->adapter_name);
-
-    return FALSE;
-}
 
 static void base64_encode_string(const char *string, char *buffer) {
     cbase64_encodestate encode_state;
@@ -125,32 +60,223 @@ static void base64_decode_string(const char *base64_string, char *buffer) {
     buffer[length_written] = '\0';
 }
 
-const static size_t max_connection_string_length = 4 * ((JUICE_MAX_SDP_STRING_LEN - 1) / 3) + (((JUICE_MAX_SDP_STRING_LEN - 1) % 3 != 0) ? 4 : 0);
+const static size_t max_description_length = JUICE_MAX_SDP_STRING_LEN - 1;
+const static size_t max_description_base64_length = 4 * ((max_description_length - 1) / 3) + (((max_description_length - 1) % 3 != 0) ? 4 : 0);
 
-static void acquire_local_connection_string(juice_agent_t *agent, JuiceParameters juice_parameters) {
-    juice_gather_candidates(agent);
+enum struct Page {
+    Initial,
+    Create,
+    Connect,
+    Connected
+};
 
-    WaitForSingleObject(juice_parameters.gathering_done_event, INFINITE);
+struct Context {
+    WINTUN_SESSION_HANDLE wintun_session;
 
-    char local_description[JUICE_MAX_SDP_STRING_LEN];
-    juice_get_local_description(agent, local_description, JUICE_MAX_SDP_STRING_LEN);
+    juice_agent_t *juice_agent;
 
-    char local_description_base64[max_connection_string_length + 1];
-    base64_encode_string(local_description, local_description_base64);
+    Page current_page;
 
-    printf("Your local connection string: %s\n", local_description_base64);
+    uiLabel *status_label;
+
+    uiBox *initial_page_box;
+
+    uiBox *create_page_box;
+    uiEntry *create_page_local_connection_string_entry;
+    uiEntry *create_page_remote_connection_string_entry;
+    uiButton *create_page_connect_button;
+
+    uiBox *connect_page_box;
+    uiEntry *connect_page_local_connection_string_entry;
+    uiEntry *connect_page_remote_connection_string_entry;
+    uiButton *connect_page_generate_button;
+};
+
+static void on_state_changed(juice_agent_t *agent, juice_state_t state, void *user_ptr) {
+    auto context = (Context*)user_ptr;
+
+    switch(state) {
+        case JUICE_STATE_COMPLETED: {
+            if(context->current_page == Page::Connect) {
+                uiControlHide(uiControl(context->connect_page_box));
+
+                context->current_page = Page::Connected;
+            }
+
+            uiLabelSetText(context->status_label, "Connected to peer!");
+            uiControlShow(uiControl(context->status_label));
+        } break;
+
+        case JUICE_STATE_FAILED: {
+            if(context->current_page == Page::Connect) {
+                uiControlHide(uiControl(context->connect_page_box));
+
+                context->current_page = Page::Connected;
+            }
+
+            uiLabelSetText(context->status_label, "Disconnected from peer!");
+            uiControlShow(uiControl(context->status_label));
+        } break;
+    }
 }
 
-static void acquire_remote_connection_string(juice_agent_t *agent) {
-    char remote_description_base64[max_connection_string_length + 1];
+static void on_gathering_done(juice_agent_t *agent, void *user_ptr) {
+    auto context = (Context*)user_ptr;
 
-    printf("Please enter remote connection string: ");
-    readline(remote_description_base64, max_connection_string_length + 1);
+    char local_description[max_description_length + 1];
+    juice_get_local_description(context->juice_agent, local_description, max_description_length + 1);
 
-    char remote_description[JUICE_MAX_SDP_STRING_LEN];
-    base64_decode_string(remote_description_base64, remote_description);
+    char local_description_base64[max_description_base64_length + 1];
+    base64_encode_string(local_description, local_description_base64);
 
-    juice_set_remote_description(agent, remote_description);
+    switch(context->current_page) {
+        case Page::Create: {
+            uiEntrySetText(context->create_page_local_connection_string_entry, local_description_base64);
+            uiControlEnable(uiControl(context->create_page_local_connection_string_entry));
+            uiControlEnable(uiControl(context->create_page_remote_connection_string_entry));
+            uiControlEnable(uiControl(context->create_page_connect_button));
+        } break;
+
+        case Page::Connect: {
+            uiEntrySetText(context->connect_page_local_connection_string_entry, local_description_base64);
+            uiControlEnable(uiControl(context->connect_page_local_connection_string_entry));
+        } break;
+    }
+}
+
+static void on_recv(juice_agent_t *agent, const char *data, size_t size, void *user_ptr) {
+    auto context = (Context*)user_ptr;
+
+    auto packet = WintunAllocateSendPacket(context->wintun_session, (DWORD)size);
+
+    memcpy(packet, data, size);
+
+    WintunSendPacket(context->wintun_session, packet);
+}
+
+struct EnumerationParameters {
+    bool adapter_found;
+    WCHAR adapter_name[MAX_ADAPTER_NAME];
+};
+
+static BOOL CALLBACK wintun_adapter_enumeration_callback(WINTUN_ADAPTER_HANDLE adapter, LPARAM parameter) {
+    auto parameters = (EnumerationParameters*)parameter;
+
+    parameters->adapter_found = true;
+
+    WintunGetAdapterName(adapter, parameters->adapter_name);
+
+    return FALSE;
+}
+
+static DWORD WINAPI packet_send_thread(LPVOID lpParameter) {
+    auto context = (Context*)lpParameter;
+
+    auto wintun_wait_handle = WintunGetReadWaitEvent(context->wintun_session);
+
+    while(true) {
+        DWORD packet_size;
+        auto packet = WintunReceivePacket(context->wintun_session, &packet_size);
+
+        if(packet != nullptr) {
+            if(juice_get_state(context->juice_agent) == JUICE_STATE_COMPLETED) {
+                juice_send(context->juice_agent, (const char*)packet, packet_size);
+            }
+
+            WintunReleaseReceivePacket(context->wintun_session, packet);
+        } else {
+            WaitForSingleObject(wintun_wait_handle, INFINITE);
+        }
+    }
+}
+
+int on_window_closing(uiWindow *window, void *data) {
+    uiQuit();
+
+    return true;
+}
+
+void on_create_page_connect_button_pressed(uiButton *button, void *data) {
+    auto context = (Context*)data;
+
+    auto remote_description_base64 = uiEntryText(context->create_page_remote_connection_string_entry);
+
+    if(cbase64_calc_decoded_length(remote_description_base64, strlen(remote_description_base64)) > max_description_length) {
+        uiLabelSetText(context->status_label, "Remote connection string too long");
+        uiControlShow(uiControl(context->status_label));
+
+        return;
+    }
+
+    char remote_descriptor[max_description_length + 1];
+    base64_decode_string(remote_description_base64, remote_descriptor);
+
+    if(juice_set_remote_description(context->juice_agent, remote_descriptor) != JUICE_ERR_SUCCESS) {
+        uiLabelSetText(context->status_label, "Remote connection string is invalid");
+        uiControlShow(uiControl(context->status_label));
+
+        return;
+    }
+
+    uiControlHide(uiControl(context->create_page_box));
+
+    context->current_page = Page::Connected;
+
+    uiLabelSetText(context->status_label, "Waiting for connection from peer...");
+    uiControlShow(uiControl(context->status_label));
+}
+
+void on_connect_page_generate_button_pressed(uiButton *button, void *data) {
+    auto context = (Context*)data;
+
+    auto remote_description_base64 = uiEntryText(context->connect_page_remote_connection_string_entry);
+
+    if(cbase64_calc_decoded_length(remote_description_base64, strlen(remote_description_base64)) > max_description_length) {
+        uiLabelSetText(context->status_label, "Remote connection string too long");
+        uiControlShow(uiControl(context->status_label));
+
+        return;
+    }
+
+    char remote_descriptor[max_description_length + 1];
+    base64_decode_string(remote_description_base64, remote_descriptor);
+
+    if(juice_set_remote_description(context->juice_agent, remote_descriptor) != JUICE_ERR_SUCCESS) {
+        uiLabelSetText(context->status_label, "Remote connection string is invalid");
+        uiControlShow(uiControl(context->status_label));
+
+        return;
+    }
+
+    uiControlDisable(uiControl(context->connect_page_remote_connection_string_entry));
+    uiControlDisable(uiControl(context->connect_page_generate_button));
+
+    juice_set_remote_description(context->juice_agent, remote_descriptor);
+
+    juice_gather_candidates(context->juice_agent);
+
+    uiLabelSetText(context->status_label, "Waiting for connection from peer...");
+    uiControlShow(uiControl(context->status_label));
+}
+
+void on_create_page_button_pressed(uiButton *button, void *data) {
+    auto context = (Context*)data;
+
+    uiControlHide(uiControl(context->initial_page_box));
+    uiControlShow(uiControl(context->create_page_box));
+
+    juice_gather_candidates(context->juice_agent);
+
+    context->current_page = Page::Create;
+}
+
+void on_connect_page_button_pressed(uiButton *button, void *data) {
+    auto context = (Context*)data;
+
+    uiControlHide(uiControl(context->initial_page_box));
+    uiControlShow(uiControl(context->connect_page_box));
+
+    context->current_page = Page::Connect;
 }
 
 #define IP_CONSTANT(a, b, c, d) ((uint32_t)d | (uint32_t)c << 8 | (uint32_t)b << 16 | (uint32_t)a << 24)
@@ -202,14 +328,6 @@ int main(int argument_count, char *arguments[]) {
 
     ip_address |= ip_subnet_prefix;
 
-    printf(
-        "Your IP address is %hhu.%hhu.%hhu.%hhu\n",
-        (uint8_t)(ip_address >> 24),
-        (uint8_t)(ip_address >> 16),
-        (uint8_t)(ip_address >> 8),
-        (uint8_t)ip_address
-    );
-
     NET_LUID wintun_adapter_luid;
     WintunGetAdapterLUID(wintun_adapter, &wintun_adapter_luid);
 
@@ -243,59 +361,155 @@ int main(int argument_count, char *arguments[]) {
 
     CreateUnicastIpAddressEntry(&address_row);
 
-    JuiceParameters juice_parameters;
-    juice_parameters.wintun_session = wintun_session;
-    juice_parameters.gathering_done_event = CreateEventA(nullptr, FALSE, FALSE, "gathering_done");
+    Context context;
+    context.wintun_session = wintun_session;
 
-    juice_config_t config {};
-    config.stun_server_host = "stun.stunprotocol.org";
-    config.stun_server_port = 3478;
+    juice_config_t juice_config {};
+    juice_config.stun_server_host = "stun.stunprotocol.org";
+    juice_config.stun_server_port = 3478;
 
-    config.user_ptr = (void*)&juice_parameters;
+    juice_config.user_ptr = (void*)&context;
 
-    config.cb_gathering_done = on_gathering_done;
-    config.cb_recv = on_recv;
+    juice_config.cb_state_changed = on_state_changed;
+    juice_config.cb_gathering_done = on_gathering_done;
+    juice_config.cb_recv = on_recv;
 
-    auto agent = juice_create(&config);
+    auto juice_agent = juice_create(&juice_config);
+    context.juice_agent = juice_agent;
 
-    char main_option[128];
+    CreateThread(nullptr, 0, packet_send_thread, (void*)&context, 0, nullptr);
 
-    printf("create: Create a network\n");
-    printf("connect: Connect to an existing network\n");
-    printf("Please choose an option: ");
+    uiInitOptions ui_init_options {};
+    uiInit(&ui_init_options);
 
-    readline(main_option, 128);
+    auto window = uiNewWindow("P2P VPN", 200, 300, false);
+    uiWindowSetMargined(window, true);
+    uiWindowOnClosing(window, on_window_closing, nullptr);
 
-    if(strcmp(main_option, "create") == 0) {
-        acquire_local_connection_string(agent, juice_parameters);
+    auto main_box = uiNewVerticalBox();
+    uiWindowSetChild(window, uiControl(main_box));
+    uiBoxSetPadded(main_box, true);
 
-        acquire_remote_connection_string(agent);
-    } else if(strcmp(main_option, "connect") == 0) {
-        acquire_remote_connection_string(agent);
+    { // Local IP address
+        auto box = uiNewHorizontalBox();
+        uiBoxAppend(main_box, uiControl(box), false);
+        uiBoxSetPadded(box, true);
 
-        acquire_local_connection_string(agent, juice_parameters);
-    } else {
-        fprintf(stderr, "ERROR: Unknown option '%s'\n", main_option);
+        auto label = uiNewLabel("Your IP address is:");
+        uiBoxAppend(box, uiControl(label), false);
 
-        return 1;
+        auto entry = uiNewEntry();
+        uiBoxAppend(box, uiControl(entry), true);
+        uiEntrySetReadOnly(entry, true);
+
+        char ip_address_text[32];
+        sprintf_s(
+            ip_address_text,
+            32,
+            "%hhu.%hhu.%hhu.%hhu",
+            (uint8_t)(ip_address >> 24),
+            (uint8_t)(ip_address >> 16),
+            (uint8_t)(ip_address >> 8),
+            (uint8_t)ip_address
+        );
+        uiEntrySetText(entry, ip_address_text);
     }
 
-    auto wintun_wait_handle = WintunGetReadWaitEvent(wintun_session);
+    { // Initial page
+        auto page_box = uiNewHorizontalBox();
+        uiBoxAppend(main_box, uiControl(page_box), false);
+        uiBoxSetPadded(page_box, true);
 
-    while(true) {
-        DWORD packet_size;
-        auto packet = WintunReceivePacket(wintun_session, &packet_size);
+        auto create_button = uiNewButton("Create");
+        uiBoxAppend(page_box, uiControl(create_button), false);
+        uiButtonOnClicked(create_button, on_create_page_button_pressed, (void*)&context);
 
-        if(packet != nullptr) {
-            if(juice_get_state(agent) == JUICE_STATE_COMPLETED) {
-                juice_send(agent, (const char*)packet, packet_size);
-            }
+        auto connect_button = uiNewButton("Connect");
+        uiBoxAppend(page_box, uiControl(connect_button), false);
+        uiButtonOnClicked(connect_button, on_connect_page_button_pressed, (void*)&context);
 
-            WintunReleaseReceivePacket(wintun_session, packet);
-        } else {
-            WaitForSingleObject(wintun_wait_handle, INFINITE);
-        }
+        context.initial_page_box = page_box;
+        context.current_page = Page::Initial;
     }
+
+    { // Create page
+        auto page_box = uiNewVerticalBox();
+        uiBoxAppend(main_box, uiControl(page_box), false);
+        uiBoxSetPadded(page_box, true);
+        uiControlHide(uiControl(page_box));
+
+        auto local_connection_string_label = uiNewLabel("Your connection string (send this to your peer)");
+        uiBoxAppend(page_box, uiControl(local_connection_string_label), false);
+
+        auto local_connection_string_entry = uiNewEntry();
+        uiBoxAppend(page_box, uiControl(local_connection_string_entry), false);
+        uiControlDisable(uiControl(local_connection_string_entry));
+        uiEntrySetReadOnly(local_connection_string_entry, true);
+        uiEntrySetText(local_connection_string_entry, "Loading...");
+
+        context.create_page_local_connection_string_entry = local_connection_string_entry;
+
+        auto remote_connection_string_label = uiNewLabel("Their connection string (your peer will send this to you)");
+        uiBoxAppend(page_box, uiControl(remote_connection_string_label), false);
+
+        auto remote_connection_string_entry = uiNewEntry();
+        uiBoxAppend(page_box, uiControl(remote_connection_string_entry), false);
+        uiControlDisable(uiControl(remote_connection_string_entry));
+
+        context.create_page_remote_connection_string_entry = remote_connection_string_entry;
+
+        auto connect_button = uiNewButton("Connect to Peer");
+        uiBoxAppend(page_box, uiControl(connect_button), false);
+        uiButtonOnClicked(connect_button, on_create_page_connect_button_pressed, (void*)&context);
+        uiControlDisable(uiControl(connect_button));
+
+        context.create_page_connect_button = connect_button;
+
+        context.create_page_box = page_box;
+    }
+
+    { // connect page
+        auto page_box = uiNewVerticalBox();
+        uiBoxAppend(main_box, uiControl(page_box), false);
+        uiBoxSetPadded(page_box, true);
+        uiControlHide(uiControl(page_box));
+
+        auto remote_connection_string_label = uiNewLabel("Their connection string (your peer will send this to you)");
+        uiBoxAppend(page_box, uiControl(remote_connection_string_label), false);
+
+        auto remote_connection_string_entry = uiNewEntry();
+        uiBoxAppend(page_box, uiControl(remote_connection_string_entry), false);
+
+        context.connect_page_remote_connection_string_entry = remote_connection_string_entry;
+
+        auto connect_button = uiNewButton("Generate connecting string");
+        uiBoxAppend(page_box, uiControl(connect_button), false);
+        uiButtonOnClicked(connect_button, on_connect_page_generate_button_pressed, (void*)&context);
+
+        auto local_connection_string_label = uiNewLabel("Your connection string (send this to your peer)");
+        uiBoxAppend(page_box, uiControl(local_connection_string_label), false);
+
+        auto local_connection_string_entry = uiNewEntry();
+        uiBoxAppend(page_box, uiControl(local_connection_string_entry), false);
+        uiControlDisable(uiControl(local_connection_string_entry));
+        uiEntrySetReadOnly(local_connection_string_entry, true);
+
+        context.connect_page_local_connection_string_entry = local_connection_string_entry;
+
+        context.connect_page_generate_button = connect_button;
+
+        context.connect_page_box = page_box;
+    }
+
+    auto error_label = uiNewLabel("");
+    uiBoxAppend(main_box, uiControl(error_label), false);
+    uiControlHide(uiControl(error_label));
+
+    context.status_label = error_label;
+
+    uiControlShow(uiControl(window));
+
+    uiMain();
 
     return 0;
 }
